c1c4500298d18f035ce9adb88a07ea0619486311
Riddhi K Singh <Riddhi.Singh@in.ey.com>
2025-03-12 13:02:32 +0530

    revert

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -4,42 +4,38 @@ import DropzoneButton from '../dropzone/Dropzone';
 import ReviewSubmission from './ReviewSubmission';
 import { OrbisData } from './OrbisData';
 import Results from './Results';
-import { useAppContext } from '../contextAPI/AppContext';
 
 export default function Steps() {
   const [active, setActive] = useState(0);
   const nextStep = () =>
     setActive((current) => (current < 4 ? current + 1 : current));
 
-  // Removed sessionId since it's not used
-  // const { sessionId } = useAppContext();
-
   return (
-    <Stepper active={active} my="xl" wrap={false} onStepClick={setActive}>
+    <Stepper active={active} my='xl' wrap={false} onStepClick={setActive}>
       <Stepper.Step
-        label="Upload Suppliers"
-        description="Import your supplier list to get started"
+        label='Upload Suppliers'
+        description='Import your supplier list to get started'
       >
-        <Space h="xl" />
+        <Space h='xl' />
         <DropzoneButton nextStep={nextStep} />
       </Stepper.Step>
       <Stepper.Step
-        label="Review Submission"
-        description="Confirm and finalize the uploaded list"
+        label='Review Submission'
+        description='Confirm and finalize the uploaded list'
       >
-        <Space h="xl" />
+        <Space h='xl' />
         <ReviewSubmission nextStep={nextStep} />
       </Stepper.Step>
       <Stepper.Step
-        label="Supplier Name Validation"
-        description="Accept/Reject Suggestions"
+        label='Supplier Name Validation'
+        description='Accept/Reject Suggestions'
       >
-        <Space h="xl" />
+        <Space h='xl' />
         <OrbisData nextStep={nextStep} />
       </Stepper.Step>
       <Stepper.Step
-        label="Review Results"
-        description="View detailed results and download reports"
+        label='Review Results'
+        description='View detailed results and download reports'
       >
         <Results />
       </Stepper.Step>
