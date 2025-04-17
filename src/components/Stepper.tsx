aadd0d36a98945cdb3c8293a20266be4022f38aa
Riddhi K Singh <Riddhi.Singh@in.ey.com>
2025-04-17 15:19:50 +0530

    update

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Stepper, Space } from '@mantine/core';
 import { unstable_usePrompt } from 'react-router';
 
